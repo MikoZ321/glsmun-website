430586b33d5c3763a7c8a6b7d4902ff9efa4bfb8
MikoZ321 <0123456789miko@gmail.com>
2023-05-14 00:39:26 +0200

    Add mobile responsive navbar

---
+++ script.js
@@ -0,0 +1,22 @@
+const body = document.querySelector('body');
+const navbar = document.querySelector(".navbar");
+const hamburger = document.querySelector("#hamburger");
+const navlinks = document.querySelectorAll(".navLink");
+
+hamburger.addEventListener("click", function () {
+
+    if (navbar.classList.contains("responsive")) {
+        console.log("responsive");
+        navbar.classList.remove("responsive");
+        body.classList.remove("noscroll");
+    }
+    else {
+        console.log("not responsive");
+        navbar.classList.add("responsive");
+        body.classList.add("noscroll");
+    }
+})
+
+navlinks.forEach(navLink => navLink.addEventListener('click', function () {
+    body.classList.remove("noscroll");
+}))
